3e3c2fc162dd4a06b849e7afe84aa9ed5531e823
Ajeethkumar-r <rajeethkumarind01@gmail.com>
2021-11-03 15:43:03 +0530

    fix: sercurity issues

---
+++ gomovie/src/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.scss';
+import '@material-ui/core';
+import '@material-ui/icons';
+import { AcUnit } from '@material-ui/icons';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <AcUnit />
+    </div>
+  );
+};
+
+export default Home;
