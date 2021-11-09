8181a6f5ccc8f2670c8309554fd7a3de1439856d
Ajeethkumar-r <rajeethkumarind01@gmail.com>
2021-11-09 05:56:14 +0530

    feat: Featured movie and series

--- gomovie/src/components/featured/Featured.jsx
+++ gomovie/src/components/featured/Featured.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import './featured.scss';
 import { InfoOutlined, PlayArrow } from '@material-ui/icons';
 
-const Featured = () => {
+export default function Featured({ type }) {
   return (
     <div className='featured'>
+      {type && (
+        <div className='category'>
+          <span>{type === 'movie' ? 'Movies' : 'Series'}</span>
+          <select name='genere' id='genere'>
+            <option>Genere</option>
+            <option value='adventure'>Adventure</option>
+            <option value='comedy'>Comedy</option>
+            <option value='crime'>Crime</option>
+            <option value='fantasy'>Fantasy</option>
+            <option value='historical'>Historical</option>
+            <option value='horror'>Horror</option>
+            <option value='romance'>Romance</option>
+            <option value='sci-fi'>Sci-fi</option>
+            <option value='thriller'>Thriller</option>
+            <option value='western'>Western</option>
+            <option value='animation'>Animation</option>
+            <option value='drama'>Drama</option>
+            <option value='documentary'>Documentary</option>
+          </select>
+        </div>
+      )}
       <img
         src='https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
         alt=''
@@ -17,7 +38,9 @@ const Featured = () => {
         <span className='desc'>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores
           accusamus totam at officia maxime dolor nihil perspiciatis quasi natus
-          voluptate.
+          voluptate.Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Cum numquam magnam sapiente similique fugit doloribus nihil illo eum,
+          nulla voluptate.
         </span>
         <div className='buttons'>
           <button className='play'>
@@ -32,6 +55,4 @@ const Featured = () => {
       </div>
     </div>
   );
-};
-
-export default Featured;
+}
