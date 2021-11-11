08443332af2ebc12501a90e01e9e4735077e800b
Ajeethkumar-r <rajeethkumarind01@gmail.com>
2021-11-11 13:31:44 +0530

    feat: Sliding feature

--- gomovie/src/components/list/List.jsx
+++ gomovie/src/components/list/List.jsx
@@ -1,5 +1,53 @@
 import './list.scss';
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from '@material-ui/icons';
+import ListItem from '../listItem/ListItem';
+import { useRef, useState } from 'react';
 
 export default function List() {
-  return <div className='list'>list</div>;
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === 'left' && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === 'right' && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+  };
+  return (
+    <div className='list'>
+      <div className='listTitle'>Continue to watch</div>
+      <div className='wrapper'>
+        <ArrowBackIosOutlined
+          className='sliderArrow left'
+          onClick={() => handleClick('left')}
+          style={{ display: !isMoved && 'none' }}
+        />
+        <div className='container' ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlined
+          className='sliderArrow right'
+          onClick={() => handleClick('right')}
+        />
+      </div>
+    </div>
+  );
 }
